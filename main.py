46d6bf6eae2b90f54fd2f8e696a038dc26d2f965
Gabriela Madalska <gabriela.madalska@gmail.com>
2024-10-28 15:46:29 +0100

    Init web app

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+app = Flask(__name__) # Class from flask module, represents web app
+
+@app.route('/') # Function that handles queries to the main page
+def weather():
+    return "Hello, World!"
+
+
+
+
+if __name__ == '__main__':
+    app.run()
