30d4241cf57251cbe1344207211dbf85d81b5134
Gabriela Madalska <gabriela.madalska@gmail.com>
2024-10-30 12:37:14 +0100

    Redirect from home to /weather

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from dotenv import load_dotenv
 import os
 import requests
@@ -6,60 +6,24 @@ import requests
 # Load env variables from .env file
 load_dotenv()
 
-app = Flask(__name__) # Class from flask module, represents web app
-
 API_KEY=os.getenv('API_KEY')
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-# @app.route('/weather', methods=['GET'])
-# def get_weather():
-#     city = request.args.get('city')
-#     if not city:
-#         return jsonify({'error': 'City parameter is missing'}), 400
 
-#     # URL do zapytania do API OpenWeatherMap
-#     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pl"
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    if request.method == 'POST':
+        city = request.form.get('city')
 
-#     # Wysłanie zapytania do API
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         data = response.json()
-#         weather_data = {
-#             'city': data['name'],
-#             'temperature': data['main']['temp'],
-#             'description': data['weather'][0]['description']
-#         }
-#         return jsonify(weather_data)
-#     else:
-#         return jsonify({'error': 'City not found'}), 404
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-@app.route('/weather', methods=['GET'])
-def getWeather():
-    city = request.args.get('city')
-    if not city:
-        return jsonify({'error': 'City parameter is missing'}), 400
-
-    # URL do zapytania do API OpenWeatherMap
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pl"
-
-    # Wysłanie zapytania do API
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        weather_data = {
-            'city': data['name'],
-            'temperature': data['main']['temp'],
-            'description': data['weather'][0]['description']
-        }
-        print("Weather Data Backend:", weather_data)
-        return jsonify(weather_data)
+        return render_template('weather.html', city=city)
+    
     else:
-        return jsonify({'error': 'City not found'}), 404
-
+        #return "Proszę przesłać nazwę miasta przez formularz na stronie głównej.", 405
+        return redirect(url_for('home'))
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,29 +1,3 @@
-console.log("JavaScript file loaded");
-async function getWeather() {
-    console.log("1. getWeather function called"); // Dodaj tę linię
-    const city = document.getElementById('city').value;
-    if (!city) {
-        alert("Please enter a city name.");
-        return;
-    }
-
-    try {
-        const response = await fetch(`http://localhost:5000/weather?city=${city}`);
-        console.log("Response Status:", response.status);
-        const data = await response.json();
-        console.log("Data received:", data);
-        const weatherDiv = document.getElementById('weather-result');
-        
-        if (response.ok) {
-            weatherDiv.innerHTML = `
-                <h2>Weather in ${data.city}</h2>
-                <p>Temperature: ${data.temperature}°C</p>
-                <p>Describe: ${data.description}</p>
-            `;
-        } else {
-            weatherDiv.innerHTML = `<p>${data.error}</p>`;
-        }
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
+document.getElementById("submitBtn").addEventListener("click", function() {
+    window.location.href = "/weather";
+});
